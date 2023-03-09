5d313cfa9d9a8e5ba267aa46628ba18af921d633
KMzaq <kimminsu3544@gmail.com>
2023-03-09 22:19:08 +0900

    Movement And Attack
    기본 이동이랑 공격 대충 추가함
    쉬프트 - 달리기  좌클릭 - 공격
    나중에 패턴넣고 수정할 예정

--- ProjectRPG/Assets/02.Scripts/TestPlayer.cs
+++ ProjectRPG/Assets/02.Scripts/TestPlayer.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public class TestPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator mAnimator;
+
+    public float speed;
+
+    float hAxis;
+    float vAxis;
+
+    Vector3 moveVec;
+
     void Start()
     {
-        
+        mAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        transform.position += moveVec.normalized * (Input.GetKey(KeyCode.LeftShift) ? speed * 3 : speed) * Time.deltaTime;
+        transform.Rotate(0, hAxis * 180 * Time.deltaTime, 0);
+    }
     void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = this.gameObject.transform.forward * vAxis;
+
+        mAnimator.SetBool("IsMove", vAxis != 0 || hAxis != 0);
+        mAnimator.SetBool("IsRun", Input.GetKey(KeyCode.LeftShift));
+        mAnimator.SetFloat("DirX", hAxis);
+        mAnimator.SetFloat("DirZ", vAxis);
+
+        mAnimator.SetBool("IsAttack", Input.GetMouseButtonDown(0));
+    }
+
+    private void LateUpdate()
     {
         
     }
